7eeb9105110e1ec3c91780411d4ae2eb4ec56b28
achuthan-18 <167844870+achuthan-18@users.noreply.github.com>
2024-09-30 23:16:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const display = document.getElementById("display");
+
+function append(input){
+    if(display.value == "Error" || display.value == "Infinity" || display.value == "undefined" || display.value == '0')  {
+    display.value = input;
+    }
+    else{
+        display.value += input;
+    }
+
+    
+}
+    
+function erase(){
+    display.value = display.value.toString().slice(0,-1);
+}
+
+
+
+function eraseall(){
+    display.value ="";
+}
+
+function result(){
+    try{
+    display.value = eval(display.value);
+    }
+
+    catch(error){
+        display.value = "Error";
+
+
+    }
+}
